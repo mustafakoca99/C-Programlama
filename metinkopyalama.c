41646c0117cdb206c981be3c68c09c28c8bc4c5e
Mustafa Koca <54147948+mustafakoca99@users.noreply.github.com>
2019-08-31 21:19:59 +0300

    Add files via upload

---
+++ metinkopyalama.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char mustafa[100];
+	char halil[100];
+	printf("gir lan gavat:",mustafa);
+	gets(mustafa);
+	str(halil,mustafa);
+	printf("%s",halil);
+	return 0;
+		}
