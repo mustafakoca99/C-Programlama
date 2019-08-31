c30c17c69350bad0a97ec38d9b116bea7e1c1a57
Mustafa Koca <54147948+mustafakoca99@users.noreply.github.com>
2019-08-31 21:16:28 +0300

    Add files via upload

---
+++ büyük küçük.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int x=10;
+    int y,z;
+    printf("sayi:",y);
+    scanf("%d",&y);
+    if(x>y)
+    {
+    	printf("sayi 10'dan kucuk");
+	}
+	else if(y>x)
+	{
+		printf("sayi 10'dan buyuk");
+	}
+	return 0;
+}
+	system("pause");
