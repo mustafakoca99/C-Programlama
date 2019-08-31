83499142a8cc34e2b9c7602b4d5f5c4426915c43
Mustafa Koca <54147948+mustafakoca99@users.noreply.github.com>
2019-08-31 21:16:47 +0300

    Add files via upload

---
+++ dosyaacmaicineverigirme.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+  FILE *fp1;
+  int id1;
+
+  if ((fp1 = fopen ("dosya1.txt", "w")) == NULL) 
+  {
+      printf("Dosya açma hatasý!");
+      exit(1);
+  }
+
+  for (id1=0; id1<10; id1++) 
+  fputc ('a', fp1);
+  fclose(fp1);
+
+  if ((fp1 = fopen ("dosya1.txt", "r")) == NULL) 
+  {
+      printf("Dosya açma hatasý!");
+      exit(1);
+  }
+
+  for (id1=0; id1<10; id1++) 
+  printf("%c", fgetc(fp1));
+  fclose(fp1);
+}
